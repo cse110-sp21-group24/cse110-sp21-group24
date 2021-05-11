f0bb793a91d7045eeeef943ccdd8205d44b7e845
Nicholas Masumoto <nmasumot@ucsd.edu>
2021-05-10 17:57:42 -0700

    test codeclimate

---
+++ test.js
@@ -0,0 +1,24 @@
+function function2(a,b,c,d,e,f){
+    let x = a+b+c+d+e+f;
+    return x;
+}
+
+function function3(a,b,c,d,e,f){
+    let x = a+b+c+d+e+f;
+    return x;
+}
+
+function function4(a,b,c,d,e,f){
+    let x = a+b;
+    return x;
+}
+
+function function5(a,b,c,d,e,f){
+    let x = a+b+c;
+    return x;
+}
+
+function function6(a,b,c,d,e,f){
+    let x = a+b+c+d;
+    return x;
+}

5c43f82773078b5338a2694637b6df1bba76f92d
Nicholas Masumoto <nmasumot@ucsd.edu>
2021-05-20 11:17:07 -0700

    take out test.js and add another .js

---
+++ public/scripts/doNothing.js
@@ -0,0 +1,19 @@
+/**
+ * This class is a class that does nothing
+ */
+
+class doNothing {
+    constructor() {
+        this.empty = [];
+    }
+
+    /**
+     * Return nothing
+     * @return {String} nothing
+     */
+
+    returnNothing() {
+        return this.empty[0];
+    }
+
+}

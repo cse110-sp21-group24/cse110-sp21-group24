658667580425e4ca81ceb0f7a29e1c67e148319c
lynguyxn <kln014@ucsd.edu>
2021-05-23 02:24:23 -0700

    added css

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -4,6 +4,17 @@ const colorThief = new ColorThief();
 
 let img = document.getElementById("output");
 
+console.log(document.styleSheets);
+
+// let colorArray = [];
+//   $("*").each(function(){
+//     let color = $(this).css("color");
+//     if(colorArray.indexOf(color) == -1) {
+//       colorArray.push(color);
+//     }
+//   });
+//   console.log(colorArray);
+
 img.addEventListener('load', function() {
   const colors = colorThief.getPalette(img,4);
   document.getElementById("color1").style = `background-color: rgb(${colors[0]})`

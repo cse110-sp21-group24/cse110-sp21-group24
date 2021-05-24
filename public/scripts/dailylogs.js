23dbd408b3441edc62384ed08e26b8de20550f17
prashanthrajan <prashanthrajan03@gmail.com>
2021-05-24 11:43:17 -0700

    Checkbox feature implemented with editing

--- public/scripts/dailylogs.js
+++ public/scripts/dailylogs.js
@@ -4,3 +4,26 @@ document.getElementById("mon-add").addEventListener('click', () => {
     item.innerHTML = "ADD ENTRY"
     document.getElementById("mon").getElementsByTagName('ul')[0].appendChild(item);
 });
+
+document.getElementById("tue-add").addEventListener('click', () => {
+    let  lab = document.createElement('label');
+    lab.innerHTML = "NEW"
+    lab.classList.add("log");
+    lab.setAttribute("contenteditable", "true");
+    let inp = document.createElement('input');
+    inp.setAttribute("type", "checkbox");
+    let box = document.createElement("span");
+    box.classList.add("checkmark");;
+    lab.appendChild(inp);
+    lab.appendChild(box);
+    document.getElementById("tue").appendChild(lab);
+});
+
+
+/*
+document.getElementsByTagName("li").forEach(item => {
+    item.addEventListener('dblclick', () => {
+        item.style.text.decoration = "line-through";
+    })
+});
+*/

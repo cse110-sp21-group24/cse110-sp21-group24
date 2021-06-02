4a04e91647caa5c06d4897bce8df9a02609bba8d
Nicholas Masumoto <nmasumot@ucsd.edu>
2021-06-01 21:37:52 -0700

    Saving With Local Storage

--- public/scripts/dailyLog.js
+++ public/scripts/dailyLog.js
@@ -71,4 +71,323 @@
       document.getElementById("dropBody").appendChild(load);
     }
   })
+
+  // retrieve added bullets
+  let bullets = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+      if (key.startsWith("mon")) {
+          bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("tue")) {
+          bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("wed")) {
+        bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("thu")) {
+        bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("fri")) {
+        bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("sat")) {
+        bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+      if (key.startsWith("sun")) {
+        bullets.push(JSON.parse(localStorage.getItem(key)));
+      }
+  }
+  bullets.sort((a, b) => (a.id > b.id) ? 1 : -1);
+  console.log(bullets);
+  bullets.forEach(elem => {
+    let bullet = createDropDown();
+    let bulletDropDown = addDropDownImages();
+    let bulletItem = document.createElement('li');
+    bulletItem.contentEditable = "true";
+    bulletItem.innerHTML = elem.entry;
+    bulletItem.classList.add(elem.bulletType);
+    bullet.appendChild(bulletItem);
+    bullet.appendChild(bulletDropDown);
+    let list = document.getElementById(elem.day).getElementsByTagName('ul')[0];
+    bullet.id = elem.id;
+    list.appendChild(bullet);
+  });
+
+});
+
+document.getElementById("mon-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("mon").getElementsByTagName('ul')[0];
+  dropDown.id = "mon" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("tue-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("tue").getElementsByTagName('ul')[0];
+  dropDown.id = "tue" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("wed-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("wed").getElementsByTagName('ul')[0];
+  dropDown.id = "wed" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("thu-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("thu").getElementsByTagName('ul')[0];
+  dropDown.id = "thu" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("fri-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("fri").getElementsByTagName('ul')[0];
+  dropDown.id = "fri" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("sat-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("sat").getElementsByTagName('ul')[0];
+  dropDown.id = "sat" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+document.getElementById("sun-add").addEventListener('click', () => {
+  let item = document.createElement('li');
+  item.contentEditable = "true";
+  item.innerHTML = "ADD ENTRY";
+  item.classList.add('task-list');
+  let dropDown = createDropDown();
+  let myDropDown = addDropDownImages();
+  dropDown.appendChild(item);
+  dropDown.appendChild(myDropDown);
+  let list = document.getElementById("sun").getElementsByTagName('ul')[0];
+  dropDown.id = "sun" + list.childNodes.length;
+  list.appendChild(dropDown);
+  saveToLC(dropDown);
+  console.log(dropDown);
+});
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropBtn')) {
+    var dropdowns = document.getElementsByClassName("dropDown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+  
+  if(event.target.classList == 'task-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }else if(event.target.classList == 'event-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }else if(event.target.classList == 'important-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }else if(event.target.classList == 'inspiration-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }else if(event.target.classList == 'note-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }else if(event.target.classList == 'checkMark-list'){
+    var dropDown = event.target.parentElement.childNodes[1];
+    dropDown.classList.toggle('show');
+  }
+
+  if(event.target.classList == 'taskImage'){
+    changeBulletIcon(event.target, 'task-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'eventImage'){
+    changeBulletIcon(event.target, 'event-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'importantImage'){
+    changeBulletIcon(event.target, 'important-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'inspirationImage'){
+    changeBulletIcon(event.target, 'inspiration-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'noteImage'){
+    changeBulletIcon(event.target, 'note-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'checkMarkImage'){
+    changeBulletIcon(event.target, 'checkMark-list');
+    saveChangedIcon(event.target);
+  }else if(event.target.classList == 'deleteImage'){
+    deleteBulletPoint(event.target);
+    deleteBulletIcon(event.target);
+  }else if(event.target.classList == 'saveIconImage'){
+    saveChangedIcon(event.target);
+  }
+
+  
+}
+
+var days = document.getElementsByTagName('h2');
+
+var day = document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0];
+
+function putBack(){
+  if (day.innerHTML == "Monday") {
+    document.getElementById("monContainer").appendChild(document.getElementById("mon"));
+    document.getElementById("monCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else if (day.innerHTML == "Tuesday") {
+    document.getElementById("tueContainer").appendChild(document.getElementById("tue"));
+    document.getElementById("tueCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else if (day.innerHTML == "Wednesday") {
+    document.getElementById("wedContainer").appendChild(document.getElementById("wed"));
+    document.getElementById("wedCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else if (day.innerHTML == "Thursday") {
+    document.getElementById("thuContainer").appendChild(document.getElementById("thu"));
+    document.getElementById("thuCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else if (day.innerHTML == "Friday") {
+    document.getElementById("friContainer").appendChild(document.getElementById("fri"));
+    document.getElementById("friCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else if (day.innerHTML == "Saturday") {
+    document.getElementById("satContainer").appendChild(document.getElementById("sat"));
+    document.getElementById("satCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+  else {
+    document.getElementById("sunContainer").appendChild(document.getElementById("sun"));
+    document.getElementById("sunCurr").classList.remove("current");
+    day.innerHTML = "";
+  }
+}
+
+days[0].addEventListener('click', () => {
+
+  putBack();
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("mon"));
+  document.getElementById("monCurr").classList.add("current");
+  day.innerHTML = "Monday";
+});
+
+days[1].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Tuesday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("tue"));
+  document.getElementById("tueCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Tuesday";
+});
+
+days[2].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Wednesday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("wed"));
+  document.getElementById("wedCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Wednesday";
+});
+
+days[3].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Thursday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("thu"));
+  document.getElementById("thuCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Thursday";
+});
+
+days[4].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Friday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("fri"));
+  document.getElementById("friCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Friday";
+});
+
+days[5].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Saturday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("sat"));
+  document.getElementById("satCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Saturday";
+});
+
+days[6].addEventListener('click', () => {
+
+  putBack();
+  day.innerHTML = "Sunday";
+  document.querySelector("[class='bigDayContent']").appendChild(document.getElementById("sun"));
+  document.getElementById("sunCurr").classList.add("current");
+  document.querySelector("[class='bigDayContent']").getElementsByTagName('h1')[0].innerHTML = "Sunday";
 });

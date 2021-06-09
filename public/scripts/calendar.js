7c59321f89aa7dfa4310955e8d0010c47c7f4d68
Vardhan Agarwal <v7agarwa@ucsd.edu>
2021-06-09 19:03:31 -0400

    added quotations

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -40,9 +40,11 @@ const renderCalendar = () => {
   // need to find how many days have to be shown from the prev month
   date.setDate(1);
   const monthDays = document.querySelector(".days");
+
   // gives the last day of the current month
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
   const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
   // this will store the index of the week when 1st of that month lies
   const firstDayIndex = date.getDay();
   // this will store the index of the week when last day of that month lies
@@ -55,22 +57,25 @@ const renderCalendar = () => {
   document.querySelector(".date p").innerHTML = new Date().toDateString();
 
   // display the previous month's last few days
-  // i = firstDayIndex-1 because my calendar starts from Monday
   for (let i = firstDayIndex; i > 0; i--){
     days += `<div class = "prev-date">${prevLastDay - i + 1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++){
-    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-      days += `<div class = today>${i}</div>`;
+    
+    if (date.getMonth() === new Date().getMonth() && i === new Date().getDate()) {
+      days += `<div class = "today">${i}</div>`;
     }
     else days += `<div>${i}</div>`;
   }
+  alert(firstDayIndex);
 
   for (let j = 1; j <= nextDays; j++){
     days += `<div class = "next-date">${j}</div>`;
     monthDays.innerHTML = days;
   }
+
+  
 
 }
 
